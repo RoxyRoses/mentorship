d4fda4675c3472110751e15945067d22c5c11053
Roxyroses <sjennifersarah@gmail.com>
2024-11-09 20:28:33 +0000

    adicionar editar e excluir mentora

--- app/controllers/mentoras_controller.rb
+++ app/controllers/mentoras_controller.rb
@@ -16,12 +16,20 @@ class MentorasController < ApplicationController
   end
 
   def edit
+    @mentora = Mentora.find(params[:id])
   end
 
   def update
+    @mentora = Mentora.find(params[:id])
+    @mentora.assign_attributes(mentora_params)
+    @mentora.save!
+    redirect_to mentora_path(@mentora)
   end
 
   def destroy
+    @mentora = Mentora.find(params[:id])
+    @mentora.destroy!
+    redirect_to mentoras_path
   end
 
   private
